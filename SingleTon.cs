e2c05d5747624eae4d31cf69e3ad0fa664cfadaf
王子川 <119828996+wangzichuan123@users.noreply.github.com>
2024-04-18 18:16:08 +0800

    单例模式

---
+++ SingleTon.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 单例脚本
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class SingleTon<T> :MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>(true);
+                if(instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    instance = obj.AddComponent<T>();
+                }
+                else
+                {
+                    if (!instance.gameObject.activeSelf)
+                    {
+                        GameObject obj = new GameObject(typeof(T).Name);
+                        instance = obj.AddComponent<T>(); 
+                    }
+                }
+            }
+
+            return instance;
+        }
+    }
+}
+
+public class SingleTonByNew<T> where T : new()
+{
+    private static T instance;
+    public static T Instance 
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new T();
+            }
+
+            return instance;
+        }
+    }
+
+}
+
